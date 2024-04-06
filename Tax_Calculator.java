a9455a90ee2799f281b51865e050878a013caf34
Ömer Turan <93642335+trnomer2534@users.noreply.github.com>
2024-04-06 15:49:52 +0300

    Update and rename TaxCalculator.java to Tax_Calculator.java

--- Tax_Calculator.java
+++ Tax_Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TaxCalculator {
+public class Tax_Calculator {
     public static void main(String[] args) {
         double price, priceWithTax, tax = 0.18;
 
